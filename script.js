46ca06472b76ef89d7c97f11d754def7f6033851
Chidimma <chidimmaofodum@gmail.com>
2022-10-12 22:35:53 +0100

    final update

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-
 const allEpisodes = getAllEpisodes();
 const rootElem = document.getElementById("root");
 
-function displayEpisodes(object) {
-  object.forEach((x) => {
+//creating a function that takes an array and displays the episodes
+
+function displayEpisodes(arrayOfEpisodes) {
+    arrayOfEpisodes.forEach((x) => {
     const episode = document.createElement("div");
     episode.setAttribute("id", "testId");
     const title = document.createElement("h3");
-    const titleContainer = document.createElement("div")
-    titleContainer.setAttribute("id", "title-container")
+    const titleContainer = document.createElement("div");
+    titleContainer.setAttribute("id", "title-container");
     const episodeImg = document.createElement("img");
     const imgContainer = document.createElement("div")
     imgContainer.setAttribute("id", "img-container")
@@ -36,20 +37,6 @@ displayEpisodes(allEpisodes);
 let search = document.getElementById("search");
 let display = document.getElementById("display");
 
-// search.addEventListener("input", () => {
-//   let newArray = [];
-//   allEpisodes.forEach((x) => {
-//     let summaryLower = x.summary.toLowerCase();
-//     if (x.name.toLowerCase().includes(search.value.toLowerCase()) || summaryLower.includes(search.value.toLowerCase()))
-//       newArray.push(x);
-//     display.innerText = `Displaying: ${newArray.length} / ${allEpisodes.length}`;
-//     rootElem.innerHTML = "";
-//     displayEpisodes(newArray);
-//   });
-// });
-
-
-
 search.addEventListener("input", () => {
  let result =  allEpisodes.filter((object) => {
     return object.name.toLowerCase().includes(search.value.toLowerCase()) || object.summary.toLowerCase().includes(search.value.toLowerCase())
@@ -59,36 +46,42 @@ search.addEventListener("input", () => {
   displayEpisodes(result)
 })
 
+// refreshing the search on click
+
 search.addEventListener("click", () => {
   rootElem.innerHTML = ""
   displayEpisodes(allEpisodes)
 })
 
-//......................Select Functionality............................................................................
+//...........................Select Functionality..................................................................
 const select = document.getElementById("select-field");
 
+//creating the options and appending to the select tag
+
 allEpisodes.forEach((x) => {
-  const option = document.createElement("option");
-  option.innerText = `S${(x.season + "").padStart(2, "0")}E${(
+   const option = document.createElement("option");
+   option.innerText = `S${(x.season + "").padStart(2, "0")}E${(
     x.number + ""
-  ).padStart(2, "0")} - ${x.name}`;
-  select.appendChild(option)
+    ).padStart(2, "0")} - ${x.name}`;
+    select.appendChild(option)
 });
+
+//adding event listener for when the select value changes
 
 select.addEventListener("change", ()=> {
   let newArray = []
- let selectValue = select.value
+  let selectValue = select.value
   selectValue = selectValue.substring(8);
- allEpisodes.forEach((x) => {
-  if (selectValue.includes(x.name)) {
+  allEpisodes.forEach((x) => {
+    if (selectValue.includes(x.name)) {
     newArray.push(x);
     rootElem.innerHTML = "";
     displayEpisodes(newArray)
-
   }
  })
-
 })
+
+//adding event listener on the refresh button
 
 document.getElementById("reset").addEventListener("click", () => {
   rootElem.innerHTML = "";
