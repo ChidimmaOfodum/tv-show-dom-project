e186d0f53d0a2087724608215d53f6d81f5bce24
Chidimma <chidimmaofodum@gmail.com>
2022-10-12 17:52:40 +0100

    Revert "changing for each to for loop"
    This reverts commit c47537fc303c6a069ba212679aad5801d908d1d3.

--- script.js
+++ script.js
@@ -38,26 +38,14 @@ let display = document.getElementById("display");
 
 search.addEventListener("keyup", () => {
   let newArray = [];
-  // allEpisodes.forEach((x) => {
-  //   let summaryLower = x.summary.toLowerCase();
-  //   if (x.name.toLowerCase().includes(search.value.toLowerCase()) || summaryLower.includes(search.value.toLowerCase()))
-  //     newArray.push(x);
-  //   display.innerText = `Displaying: ${newArray.length} / ${allEpisodes.length}`;
-  //   rootElem.innerHTML = "";
-  //   displayEpisodes(newArray);
-  // });
-
-  for (let episode of allEpisodes) {
-    let summaryLower = episode["summary"].toLowerCase();
-    if ((episode["name"]).toLowerCase().includes(search.value.toLowerCase()) || summaryLower.includes(search.value.toLowerCase())) {
-      newArray.push(episode)
-    }
+  allEpisodes.forEach((x) => {
+    let summaryLower = x.summary.toLowerCase();
+    if (x.name.toLowerCase().includes(search.value.toLowerCase()) || summaryLower.includes(search.value.toLowerCase()))
+      newArray.push(x);
     display.innerText = `Displaying: ${newArray.length} / ${allEpisodes.length}`;
     rootElem.innerHTML = "";
-    displayEpisodes(newArray)
-  }
-
-
+    displayEpisodes(newArray);
+  });
 });
 
 search.addEventListener("click", () => {
