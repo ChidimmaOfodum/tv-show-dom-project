89306d8d5599ad3fd8bfc91f21a123307920ca34
Chidimma <chidimmaofodum@gmail.com>
2022-11-05 10:45:34 +0000

    completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Defining variables
 
 const rootElem = document.getElementById("root");
 const showSelect = document.getElementById("show-select");
@@ -6,17 +6,14 @@ const allShows = getAllShows();
 const wrapper = document.createElement("div");
 wrapper.setAttribute("id", "wrapper");
 const homeIcon = document.querySelector("#homeCtn");
+const refreshIcon = document.querySelector("#rotateCtn");
 const episodeSelect = document.getElementById("select-field");
- let episodeSearch = document.getElementById("episodeSearch")
- let searchbar = document.getElementById("showSearch")
- let displayNumber = document.querySelector(".display")
- let displayNumEpi = document.getElementById("display")
+let episodeSearch = document.getElementById("episodeSearch");
+let searchbar = document.getElementById("showSearch");
+let displayNumber = document.querySelector(".display");
+let displayNumEpi = document.getElementById("display");
 
- displayNumEpi.innerText = "success"
-
-//..............creating a function that takes an array and displays the
-//episodes.......................................
-
+//creating a function that takes an array of shows and renders the shows
 function display(array) {
   rootElem.innerHTML = "";
   array.forEach((item) => {
@@ -32,11 +29,9 @@ function display(array) {
     imgContainer.setAttribute("id", "img-container");
     const summaryContainer = document.createElement("div");
     summaryContainer.setAttribute("class", "summaryContainer");
+    const summary = document.createElement("p");
     const ratingContainer = document.createElement("div");
     ratingContainer.setAttribute("class", "ratingContainer");
-    // const rating = document.createElement("p");
-    // rating.setAttribute("id", "rating");
-  
 
     const ratingContent = `<p><i class="fa-solid fa-star">
     </i><i class="fa-solid fa-star">
@@ -44,17 +39,8 @@ function display(array) {
     </i><i class="fa-solid fa-star">
     </i><i class="fa-solid fa-star">
     </i>Rating: ${item.rating.average}</p>\n
-    <p> <i class="fa-solid fa-circle"></i>Status: ${
-      item.status
-    }</p>\n<p> <i class="fa-solid fa-film"></i>Genres: ${item.genres}</p>\n<p><i class="fa-solid fa-hourglass-start"></i>Runtime: ${
-      item.runtime
-    }</p>`;
-
-   
-
+    <p> <i class="fa-solid fa-circle"></i>Status: ${item.status}</p>\n<p> <i class="fa-solid fa-film"></i>Genres: ${item.genres}</p>\n<p><i class="fa-solid fa-hourglass-start"></i>Runtime: ${item.runtime}</p>`;
     ratingContainer.innerHTML += ratingContent;
-
-    const summary = document.createElement("p");
 
     title.textContent = item.season
       ? `${item.name} - S${(item.season + "").padStart(2, "0")}E${(
@@ -64,7 +50,7 @@ function display(array) {
 
     if (item.image) episodeImg.src = item.image.original;
     summary.innerHTML = item.summary;
-    // rating.innerText = item.summary
+
     rootElem.appendChild(showContainer);
     showContainer.appendChild(titleContainer);
     titleContainer.appendChild(title);
@@ -74,141 +60,122 @@ function display(array) {
     content.appendChild(ratingContainer);
     imgContainer.appendChild(episodeImg);
     summaryContainer.appendChild(summary);
-    // ratingContainer.appendChild(rating);
-
 
     title.addEventListener("click", () => {
+      searchbar.style.display = "none"
+      episodeSearch.style.display = "block"
       if (title.textContent.includes(item.name)) {
-        showSelect.value = item.name
-        getEpisodes(item)
+        showSelect.value = item.name;
+        episodeSelect.style.display = "block";
+        getEpisodes(item);
       }
-    })
+    });
   });
 }
 
 function setUp() {
-  display(allShows)
-  episodeSearch.style.display = "none"
-  searchbar.style.display = "block"
-  let search = document.getElementById("search")
-  search.value = ""
+  display(allShows);
+  episodeSearch.style.display = "none";
+  searchbar.style.display = "block";
+  let search = document.getElementById("search");
+  search.value = "";
+  episodeSelect.style.display = "none";
   showSelect.selectedIndex = 0;
-  episodeSelect.selectedIndex = 0
-  renderDisplayNum(displayNumber, allShows, allShows)
+  episodeSelect.selectedIndex = 0;
+  renderDisplayNum(displayNumber, allShows, allShows);
 }
 
-setUp()
-
-
-
 //adding event listener to home button
-homeIcon.addEventListener("click", setUp)
-
-
+homeIcon.addEventListener("click", setUp);
 
 //select functionality
-
-let test;
-[...allShows].sort((a, b) => a.name.localeCompare(b.name))
+let allShowsCopy = [...allShows];
+allShowsCopy
+  .sort((a, b) => a.name.localeCompare(b.name))
   .forEach((x) => {
     const option = document.createElement("option");
     option.innerText = x.name;
     showSelect.appendChild(option);
   });
 
- 
-  
-  showSelect.addEventListener("change", () => {
-    let newArray = []
-    let selectValue = showSelect.value;
-     searchbar.style.display = "none"
-     episodeSearch.style.display = "block"
-    allShows.forEach((x) => {
-      if (selectValue === x.name) {
-        newArray.push(x)
-        display(newArray)
-      //  getEpisodes(x)
-      }
-    });
+showSelect.addEventListener("change", () => {
+  let newArray = [];
+  let selectValue = showSelect.value;
+  searchbar.style.display = "none";
+  episodeSearch.style.display = "block";
+  allShows.forEach((x) => {
+    if (selectValue === x.name) {
+      newArray.push(x);
+      display(newArray);
+      episodeSelect.style.display = "none";
+      renderDisplayNum(displayNumEpi, newArray, allShows);
+    }
   });
+});
 
-  // search functionality
-    searchbar.addEventListener("input", (e) => {
-      let result = allShows.filter(x => {
-      return  x.name.toLowerCase().includes(e.target.value.toLowerCase())
-      })
-      display(result)
-      renderDisplayNum(displayNumber, result, allShows)
-    });
-  
-    
+// search functionality
+searchbar.addEventListener("input", (e) => {
+  let result = allShows.filter((x) => {
+    return (
+      x.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+      x.summary.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+  });
+  display(result);
+  renderDisplayNum(displayNumber, result, allShows);
+});
 
- 
+/// defining the function that gets episodes with an api
 
-
-
-
-  
-  /// defining the function that fetches api 
-
- let allEpisodes;
-  function getEpisodes(show) {
-    let url = `https://api.tvmaze.com/shows/${show.id}/episodes`;
-    fetch(url)
-    .then (response => response.json())
-    .then(data => {
-      makePageForEpisodes(data)
+function getEpisodes(show) {
+  let url = `https://api.tvmaze.com/shows/${show.id}/episodes`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      makePageForEpisodes(data);
       renderDisplayNum(displayNumEpi, data, data);
-      episodeSelect.innerHTML = `<option>Select an episode</option>`
-      data.forEach(x => {
+      episodeSelect.innerHTML = `<option>Select an episode</option>`;
+      data.forEach((x) => {
         let option = document.createElement("option");
-        option.innerText =  `S${(x.season + "").padStart(2, "0")}E${( x.number + "").padStart(2, "0")} - ${x.name}`
+        option.innerText = `S${(x.season + "").padStart(2, "0")}E${(
+          x.number + ""
+        ).padStart(2, "0")} - ${x.name}`;
         episodeSelect.appendChild(option);
-        episodeSelect.addEventListener("change", ()=> {
-          if(episodeSelect.value.substring(9) === x.name) {
+        episodeSelect.addEventListener("change", () => {
+          if (episodeSelect.value.substring(9) === x.name) {
             let newArray = [];
-           newArray.push(x)
-           makePageForEpisodes(newArray)
-           renderDisplayNum(displayNumEpi, newArray, data)
-          } 
-        })
-      })
+            newArray.push(x);
+            makePageForEpisodes(newArray);
+            renderDisplayNum(displayNumEpi, newArray, data);
+          }
+        });
+      });
 
-      episodeSearch.addEventListener("input", (e)=> {
+      episodeSearch.addEventListener("input", (e) => {
         let result = data.filter((x) => {
           return x.name.toLowerCase().includes(e.target.value.toLowerCase());
         });
         makePageForEpisodes(result);
-        renderDisplayNum(displayNumEpi, result, data)
-        // displayNumEpi.innerText = `Displaying: ${result.length} / ${data.length}`
-      })
-     
-      //  search.addEventListener("input", () => {
-      //    let result = data.filter((x) => {
-      //      return x.name.toLowerCase().includes(search.value.toLowerCase());
-      //    });
-      //    makePageForEpisodes(result);
-      //  });
-    })
-  }
+        renderDisplayNum(displayNumEpi, result, data);
+      });
 
-  
+      refreshIcon.addEventListener("click", () => {
+        makePageForEpisodes(data);
+        episodeSelect.selectedIndex = 0;
+      });
+    });
+}
 
+function renderDisplayNum(element, subArr, mainArr) {
+  element.innerText = `Displaying: ${subArr.length} / ${mainArr.length}`;
+}
 
-
-  function renderDisplayNum(element, subArr, mainArr) {
-    element.innerText = `Displaying; ${subArr.length} / ${mainArr.length}`
-  }
-
- 
-  
-
-
-//.........making pages for episode
+//.........displaying episodes for each show pages for episode
 
 function makePageForEpisodes(array) {
-  rootElem.innerHTML = ""
-  wrapper.innerHTML = ""
+  renderDisplayNum(displayNumEpi, array, array);
+  rootElem.innerHTML = "";
+  wrapper.innerHTML = "";
   array.forEach((x) => {
     const episode = document.createElement("div");
     episode.setAttribute("id", "episode");
@@ -216,7 +183,7 @@ function makePageForEpisodes(array) {
     const titleContainer = document.createElement("div");
     titleContainer.setAttribute("class", "title-container");
     const episodeImg = document.createElement("img");
-    episodeImg.setAttribute("id", "eImg")
+    episodeImg.setAttribute("id", "eImg");
     const EImgContainer = document.createElement("div");
     EImgContainer.setAttribute("id", "EImg-container");
     const ESummary = document.createElement("p");
@@ -226,20 +193,20 @@ function makePageForEpisodes(array) {
       x.number + ""
     ).padStart(2, "0")}`;
     if (x.image) {
-      episodeImg.src = x.image.original
+      episodeImg.src = x.image.original;
     }
     ESummary.innerHTML = x.summary;
     rootElem.appendChild(wrapper);
     wrapper.appendChild(episode);
-    episode.appendChild(titleContainer)
+    episode.appendChild(titleContainer);
     titleContainer.appendChild(title);
-    episode.appendChild(EImgContainer)
-    EImgContainer.appendChild(episodeImg)
-    episode.appendChild(ESummary)
+    episode.appendChild(EImgContainer);
+    EImgContainer.appendChild(episodeImg);
+    episode.appendChild(ESummary);
   });
 }
 
-
+window.onload = setUp;
 
 // function display(array) {
 //     array.forEach((x) => {
